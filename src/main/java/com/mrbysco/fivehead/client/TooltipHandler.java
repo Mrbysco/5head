8673a8fcc01a9614e7df365a78f984bf523a6d77
Mrbysco <Mrbysco@users.noreply.github.com>
2022-07-27 23:08:33 +0200

    Port to 1.19 [push]

--- src/main/java/com/mrbysco/fivehead/client/TooltipHandler.java
+++ src/main/java/com/mrbysco/fivehead/client/TooltipHandler.java
@@ -4,8 +4,8 @@ import com.mrbysco.fivehead.FiveHead;
 import com.mrbysco.fivehead.ScaleUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,8 +17,8 @@ public class TooltipHandler {
 		if (ScaleUtil.isSkullBlock(stack) && stack.hasTag() && !stack.getTag().isEmpty() && stack.getTag().contains(FiveHead.SCALE_TAG)) {
 			CompoundTag tag = stack.getTag();
 			float scaleAddition = tag.getFloat(FiveHead.SCALE_TAG);
-			MutableComponent component = new TranslatableComponent("fivehead.enlarged.tooltip", Math.round((scaleAddition * 100) * 1000.0) / 1000.0).withStyle(ChatFormatting.GOLD);
-			component.append(new TranslatableComponent("fivehead.enlarged.tooltip2").withStyle(ChatFormatting.GOLD));
+			MutableComponent component = Component.translatable("fivehead.enlarged.tooltip", Math.round((scaleAddition * 100) * 1000.0) / 1000.0).withStyle(ChatFormatting.GOLD);
+			component.append(Component.translatable("fivehead.enlarged.tooltip2").withStyle(ChatFormatting.GOLD));
 			event.getToolTip().add(component);
 		}
 	}
