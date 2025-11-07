61fa8586b813b74d9dd3114a97e3c99adc4efef9
Mrbysco <Mrbysco@users.noreply.github.com>
2025-11-07 22:03:21 +0100

    Port to 1.21.10

--- src/main/java/com/mrbysco/fivehead/mixin/CustomHeadLayerMixin.java
+++ src/main/java/com/mrbysco/fivehead/mixin/CustomHeadLayerMixin.java
@@ -2,7 +2,7 @@ package com.mrbysco.fivehead.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mrbysco.fivehead.client.HeadHandler;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CustomHeadLayer.class)
 public class CustomHeadLayerMixin<S extends LivingEntityRenderState> {
-	@Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/blockentity/SkullBlockRenderer;getRenderType(Lnet/minecraft/world/level/block/SkullBlock$Type;Lnet/minecraft/world/item/component/ResolvableProfile;)Lnet/minecraft/client/renderer/RenderType;", shift = Shift.AFTER, ordinal = 0))
-	public void fiveheadRender(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, S renderState, float v, float v1, CallbackInfo ci) {
+	@Inject(method = "submit(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;ILnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/CustomHeadLayer;resolveSkullRenderType(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lnet/minecraft/world/level/block/SkullBlock$Type;)Lnet/minecraft/client/renderer/RenderType;", shift = Shift.AFTER, ordinal = 0))
+	public void fiveheadRender(PoseStack poseStack, SubmitNodeCollector nodeCollector, int packedLight, S renderState,
+	                           float yRot, float xRot, CallbackInfo ci) {
 		HeadHandler.resizeSkull(renderState, poseStack);
 	}
 }
